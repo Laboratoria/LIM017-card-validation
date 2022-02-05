4ae0aa7d66ca34f7234f9cce1e1f323e49c24afa
sharksumi <nay.trevejo@gmail.com>
2022-02-04 21:24:46 -0500

    IsValid mejorado

--- src/validator.js
+++ src/validator.js
@@ -1,24 +1,22 @@
 const validator = {
   isValid: function (creditCardNumber) {
-    let reverse = creditCardNumber.split("").reverse().join("");
-    let totalFinal = 0;
-    for (let posicion = 0; posicion < reverse.length; posicion++) {
-      if (posicion % 2 != 0) {
-        let doblePares = reverse[posicion] * 2;
-        reverse[posicion] * 2;
-        if (doblePares > 9) {
-          doblePares = doblePares.toString();
+    let totalFinal = 0; // contador
+    for (let posicion = creditCardNumber.length - 1; posicion >= 0; posicion--) { //lee de  n a 0 (al reves)
+      if (posicion % 2 != 0) { //detecta si es cada segundo digito
+        let doblePares = creditCardNumber[posicion] * 2; //multiplica el segundo digito
+          if (doblePares > 9 ) { //aplica  a cualquier numero de 2 digitos
+          doblePares = doblePares.toString(); //convierto a string
           doblePares = parseInt(doblePares[0]) + parseInt(doblePares[1]);
         }
-        totalFinal = totalFinal + doblePares;
+        totalFinal = totalFinal + doblePares; //se suma al total y se guarda
       } else {
-        totalFinal = totalFinal + parseInt(reverse[posicion]);
+         //si es impar lo suma y lo agrega sin hacer nada mas
+        totalFinal = totalFinal + parseInt(creditCardNumber[posicion]);
       }
     }
-    if (totalFinal % 10 == 0) {
+    if (totalFinal % 10 == 0) { // si es multiplo de 10
       return true;
-    }
-    else {
+    } else {
       return false;
     }
   },
